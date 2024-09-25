37fab10f5d634f36f440303a7a34cb4c8689cd81
RitikShah09 <ritiksbs@gmail.com>
2024-09-25 09:43:48 +0530

    Blog Controllers Updated

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,6 +1,8 @@
 const Blog = require("../models/blogModel.js");
 const { uploadOnCloudinary } = require("../utils/cloudinary.js");
 const upload = require("../middlewares/multer.js");
+const { asyncHandler } = require("../utils/asyncHandler");
+const { ApiError } = require("../utils/ApiError.js");
 
 // Route for Getting all Blogs Data
 const allBlogs = async (req, res) => {
@@ -9,25 +11,27 @@ const allBlogs = async (req, res) => {
 };
 
 const createBlog = async (req, res) => {
-  const dataWithCloudinaryImgUrl = { ...req.body, image: req.file.path };
-  const newBlog = new Blog(dataWithCloudinaryImgUrl);
+  // const dataWithCloudinaryImgUrl = { ...req.body, image: req.file.path };
+  // const newBlog = new Blog(dataWithCloudinaryImgUrl);
+  const newBlog = new Blog(req.body);
   await newBlog.save();
   res.send("success");
 };
 
 // Route to update views of a Specific blog
-const updateViews = async (req, res) => {
+const updateViews = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
-  await Blog.findByIdAndUpdate(id, req.body);
-  res.send("success");
-};
+  const blog = await Blog.findById(id);
 
-// Route to Update the Likes of a blog
-const updateLikes = async (req, res) => {
-  const { id } = req.params;
-  await Blog.findByIdAndUpdate(id, req.body);
-  res.send("success");
-};
+  if (!blog) {
+    return next(new ApiError(404, "Blog not found"));
+  }
+  // Increment views by 1
+  blog.views += 1;
+  await blog.save();
+
+  res.status(200).json({ message: "Blog views updated", blog });
+});
 
 // Route to get details of Specific Blog
 const blogDetails = async (req, res) => {
@@ -35,6 +39,44 @@ const blogDetails = async (req, res) => {
   const blog = await Blog.findOne({ slug: slug });
   res.json(blog);
 };
+
+const updateLikes = asyncHandler(async (req, res, next) => {
+  const blog = await Blog.findById(req.params.id);
+  const userId = req.userId;
+  if (!blog) {
+    return res.status(404).json({ success: false, message: "Blog not found" });
+  }
+
+  const isLiked = blog.likes.includes(userId);
+
+  let updatedBlog;
+
+  if (isLiked) {
+    // If the user has already liked the blog, unlike it
+    updatedBlog = await Blog.findByIdAndUpdate(
+      req.params.id,
+      { $pull: { likes: userId } },
+      { new: true }
+    );
+    res.status(200).json({
+      success: true,
+      updatedBlog,
+      message: "Blog unlike successfully.",
+    });
+  } else {
+    // If the user hasn't liked the blog, like it
+    updatedBlog = await Blog.findByIdAndUpdate(
+      req.params.id,
+      { $push: { likes: userId } },
+      { new: true }
+    );
+    res.status(200).json({
+      success: true,
+      updatedBlog,
+      message: "Blog liked successfully.",
+    });
+  }
+});
 
 // Serve static files from the uploads folder
 // app.use("/uploads", express.static(path.join(__dirname, "uploads")));
