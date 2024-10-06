a739067c0bcea998afeedff215489e485fa3d643
tamanhansalia <taman.hansalia@gmail.com>
2024-10-07 01:29:35 +0530

    Updated server.js userController,userRoutes to fix LandlordDashboardProfileForm

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,24 +5,32 @@ exports.getUserInfo = async (req, res) => {
   try {
     const { token } = req.query;
 
+    // Check if token exists
     if (!token) {
       return res.status(401).json("Unauthorised");
     }
+
+    // Try decoding the token
     const decoded = jwtDecode(token);
+    // console.log("Decoded Token:", decoded);
 
-    const userId = decoded.user.id;
+    // Make sure the ID exists
+    const userId = decoded.id; // Using 'id' directly based on your token structure
+    // console.log("User ID:", userId);
 
+    // Try fetching the user from MongoDB
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(404).json("user not found");
+      return res.status(404).json("User not found");
     }
 
+    // Send the user data
     const userData = {
       userId: user._id,
-      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
       email: user.email,
-      role: user.role,
-      userType: user.userType,
+      phoneNumber: user.phoneNumber,
     };
 
     res.status(200).json(userData);
@@ -31,3 +39,26 @@ exports.getUserInfo = async (req, res) => {
     res.status(500).json("Internal server error");
   }
 };
+
+exports.updateUser = async (req, res) => {
+  // console.log("Request Body: ", req.body); // Log the incoming request body
+  const { userId, firstName, lastName, email, phoneNumber } = req.body;
+
+  try {
+    const user = await User.findByIdAndUpdate(
+      userId,
+      { firstName, lastName, email, phoneNumber },
+      { new: true } // This option returns the updated document
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ message: "User updated successfully", user });
+  } catch (error) {
+    console.error("Error updating user: ", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
