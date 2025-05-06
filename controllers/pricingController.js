1aae1b3aa19971231507956aead069ab7db03474
shraddhaSarwade <shraddha154@gmail.com>
2025-05-06 23:31:09 +0530

    Added code for Pricing

--- controllers/pricingController.js
+++ controllers/pricingController.js
@@ -2,35 +2,34 @@ const nodemailer = require("nodemailer");
 const Pricing = require("../models/pricingModel");
 
 const pricingSubmit = async (req, res) => {
+  console.log("Pricing form submission request received:", req.body);
   try {
-
     const {
       firstName,
       lastName,
       phoneNumber,
-      email, 
+      email,
       stayingWith,
       profession,
       dateOfVisit,
       timeSlot,
     } = req.body;
 
-    // Sending the data through an email to the beckend team using the nodemailer 
+    console.log("Email", email);
+
+    // Sending the data through an email to the beckend team using the nodemailer
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      host: "sg2plzcpnl508365.prod.sin2.secureserver.net",
+      port: 465,
       auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_PASSWORD,
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
       },
+      secure: true,
     });
-    const mailOptions = {
-      from: {
-        name: "ToLetGlobe Form",
-        address: process.env.GMAIL_USER,
-      },
+
+    let mailOptions = {
+      from: process.env.SMTP_USER, // Sender address
       to: email, // Send email to the user who submitted the form
       subject: `Enquiry Confirmation - ToLetGlobe`,
       text: `
@@ -53,30 +52,61 @@ const pricingSubmit = async (req, res) => {
     ToLetGlobe Team
     `,
     };
-    
+
     // Send email
     await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully to user");
+
+    mailOptions = {
+      from: process.env.SMTP_USER, // Sender address
+      to: process.env.SMTP_USER, // Send email to the user who submitted the form
+      subject: `Enquiry Confirmation - ToLetGlobe`,
+      text: `
+     ${firstName} ${lastName} has submitted an enquiry,
+    
+    
+    First Name: ${firstName}
+    Last Name: ${lastName}
+    Email: ${email}
+    Phone Number: ${phoneNumber}
+    Staying With: ${stayingWith}
+    Profession: ${profession}
+    Date of Visit: ${dateOfVisit}
+    Time Slot: ${timeSlot}
+    
+    
+    Best regards,
+    ToLetGlobe Team
+    `,
+    };
+
+    await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully to backend team");
 
     // If the email is sent successfully then submit the entry into the mongodb
-    const formEntry = new Pricing({
-      firstName,
-      lastName,
-      phoneNumber,
-      email,
-      stayingWith,
-      profession,
-      dateOfVisit,
-      timeSlot,
-    });
 
-    await formEntry.save();
+    // DO NOT DELETE THIS CODE
+    // const formEntry = new Pricing({
+    //   firstName,
+    //   lastName,
+    //   phoneNumber,
+    //   email,
+    //   stayingWith,
+    //   profession,
+    //   dateOfVisit,
+    //   timeSlot,
+    // });
+
+    // await formEntry.save();
     res.status(200).send({ msg: "Form submitted successfully." });
-} catch (error) {
+  } catch (error) {
     console.error("Error sending mail or saving form:", error);
-    res.status(500).send({ msg: "Server error. Could not send email or save form." });
+    res
+      .status(500)
+      .send({ msg: "Server error. Could not send email or save form." });
   }
 };
 
 module.exports = {
-  pricingSubmit
+  pricingSubmit,
 };
