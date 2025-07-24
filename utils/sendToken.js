986cbc22e8cf5e4d39e7e7f2c6c905effd657eae
amoljaynadtodkar <02amol@gmail.com>
2025-07-24 16:56:20 +0530

    Minor bug Fixes

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -6,11 +6,9 @@ exports.sendToken = (user, statusCode, res) => {
     throw new Error("JWT_SECRET is not defined in environment variables.");
   }
 
-  const token = jwt.sign(
-    { id: user._id },
-    process.env.JWT_SECRET,
-    { expiresIn: process.env.JWT_EXPIRES_IN || "7d" }
-  );
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN || "7d",
+  });
 
   res.status(statusCode).json({
     success: true,
@@ -20,6 +18,7 @@ exports.sendToken = (user, statusCode, res) => {
       email: user.email,
       firstName: user.firstName,
       lastName: user.lastName,
+      role: user.role,
     },
   });
 };
